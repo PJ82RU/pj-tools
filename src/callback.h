d1684d0863cfd87571d8415e15c385fe3e77c339
PJ82 <project82@mail.ru>
2024-10-27 11:38:24 +0500

    Rollback

--- src/callback.h
+++ src/callback.h
@@ -43,7 +43,8 @@ namespace tools {
          * @param t_stack_depth Глубина стека
          * @param t_priority Приоритет
          */
-        Callback(uint8_t num, size_t size, const char *t_name, uint32_t t_stack_depth, UBaseType_t t_priority);
+        Callback(uint8_t num, size_t size, const char *t_name, uint32_t t_stack_depth = 1024,
+                 UBaseType_t t_priority = 15);
         ~Callback();
 
         /**
